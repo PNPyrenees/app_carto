0546ddb1082c1451be7bb18f72f9bbf452a9b0c6
PNPyrenees <ludovic.lepontois@pyrenees-parcnational.fr>
2021-10-14 08:50:25 +0000

    Correction bug filtre par altitude (oubli fichier)

--- backend/app_carto.py
+++ backend/app_carto.py
@@ -536,8 +536,7 @@ def check_obs_form_data(postdata):
                 "message": "Valeur incorrect, entier attendu"
             })
     
-    if postdata["altitude_min"] is not None and postdata["altitude_max"] is not None and altitude_min_is_valid and altitude_max_is_valid:
-    #if bool(postdata["altitude_min"]) and bool(postdata["altitude_max"]) and altitude_min_is_valid and altitude_max_is_valid:
+    if bool(postdata["altitude_min"]) and bool(postdata["altitude_max"]) and altitude_min_is_valid and altitude_max_is_valid:
         if postdata["altitude_min"] > postdata["altitude_max"]:
             error.append({
                 "field": "altitude",
