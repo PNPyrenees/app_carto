39aa489dd69030283eaef5545876afac6bda06a0
lepontois <ludovic.lepontois@pyrenees-parcnational.fr>
2025-05-14 14:02:33 +0200

    [backend] - Mise àjour des librairies python

--- backend/utils/config_schema.py
+++ backend/utils/config_schema.py
@@ -29,29 +29,30 @@ BASEMAPS = [
 
 class ConfSchema(Schema):
     APP_NAME = fields.String(required=True)
-    APP_URL = fields.String(require=True)
+    APP_URL = fields.String(required=True)
     SQLALCHEMY_DATABASE_URI = fields.String(required=True)
     SQLALCHEMY_SIG_DATABASE_URI = fields.String(required=True)
-    SQLALCHEMY_TRACK_MODIFICATIONS = fields.Boolean(missing=False)
-    GEONATURE_URL = fields.String(require=True)
-    TAXHUB_LIST_ID = fields.Integer(require=True, missing=100)
-    ID_APPLICATION = fields.Integer(require=True)
+    # Voir l'utilité de conserver cette variable de configuration
+    #SQLALCHEMY_TRACK_MODIFICATIONS = fields.Boolean(load_default=False)
+    GEONATURE_URL = fields.String(required=True)
+    TAXHUB_LIST_ID = fields.Integer(required=True)
+    ID_APPLICATION = fields.Integer(required=True)
     GEONETWORK_URL = fields.String(required=False)
     GEONETWORK_USER = fields.String(required=False)
     GEONETWORK_PASSWORD = fields.String(required=False)
     LOGO_STRUCTURE = fields.String(required=True)
-    DEBUG = fields.Boolean(missing=False)
+    DEBUG = fields.Boolean(load_default=False)
     ENV = fields.String(required=True)
-    CENTER = fields.List(fields.Float, missing=[42.922276035501696, -0.16777084451550842])
-    ZOOM_LEVEL = fields.Integer(missing=6)
-    BASEMAPS = fields.List(fields.Dict(), missing=BASEMAPS)
-    OBS_LAYER_CLASSIFICATION_BORNE = fields.List(fields.Float, missing=[0.1, 0.2, 0.4, 0.8])
-    OBS_LAYER_CLASS_COLOR = fields.List(fields.String, missing=["rgba(237, 248, 251, 0.9)", "rgba(179, 205, 227, 0.9)", "rgba(140, 150, 198, 0.9)", "rgba(136, 86, 167, 0.9)", "rgba(129, 15, 124, 0.9)"])
-    SRID = fields.Integer(require=True, missing=2154)
+    CENTER = fields.List(fields.Float, load_default=[42.922276035501696, -0.16777084451550842])
+    ZOOM_LEVEL = fields.Integer(load_default=6)
+    BASEMAPS = fields.List(fields.Dict(), load_default=BASEMAPS)
+    OBS_LAYER_CLASSIFICATION_BORNE = fields.List(fields.Float, load_default=[0.1, 0.2, 0.4, 0.8])
+    OBS_LAYER_CLASS_COLOR = fields.List(fields.String, load_default=["rgba(237, 248, 251, 0.9)", "rgba(179, 205, 227, 0.9)", "rgba(140, 150, 198, 0.9)", "rgba(136, 86, 167, 0.9)", "rgba(129, 15, 124, 0.9)"])
+    SRID = fields.Integer(required=True)
     #TEST = fields.String(required=True)
 
     @validates("ENV")
-    def check_env_value(self, value):
+    def check_env_value(self, value, **kwargs):
         env_values = ["production", "development"]
         if value not in env_values :
             raise ValidationError("Valeur incorrect. Valeurs attendues : {}".format(env_values)) 
